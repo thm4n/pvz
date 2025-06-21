238414cd249f13441290367a6b9d76fd5bdbb2e1
tamir hayne <tamir.hayne@cellebrite.com>
2025-06-21 15:54:20 +0300

    animations, Animation class and AnimationReader namespace

--- source/inc/Animation.hpp
+++ source/inc/Animation.hpp
@@ -4,24 +4,53 @@
 #include <string>
 #include <vector>
 
+#include <filesystem>
+namespace fs = std::filesystem;
+
+#include <tinyxml2.h>
+namespace xml = tinyxml2; 
+
+#include "Animation.hpp"
+
+#include "Logger.hpp"
+
+class Animation;
+
+namespace AnimationReader {
+    typedef struct {
+        int frameIndex; // Index of the frame in the animation
+        double x, y; // Position of the frame
+        double sx, sy; // Scale factors
+        double kx, ky; // Rotation angle in radians
+        double opacity; // Opacity of the frame
+        bool shown; // Whether the frame is currently shown
+        std::string image;
+    } AnimationFrame;
+
+    typedef struct {
+        std::string name;
+        std::vector<AnimationFrame> frames; // List of frames in the track
+        int frameCount; // Total number of frames in the track
+    } AnimationTrack;
+
+    Animation* readAnimation(const std::string& filePath);
+    void parseTrack(xml::XMLElement* trackElement, AnimationTrack& track);
+    bool parseTrackFrame(xml::XMLElement* frameElement, AnimationFrame& frame);
+    void printFrame(const AnimationFrame& frame);
+}
+
 class Animation {
 public:
-    Animation(int fps, int frameCount, const std::string& name) {
-        this->_fps = fps;
-        this->_frameCount = frameCount;
-        this->_duration = (float)frameCount / fps; // Calculate duration in seconds
-        this->_name = name;
-
-        this->_currentFrame = 0; // Start at the first frame
-        this->_shown = false; // Initially not shown
-    }
+    Animation(int fps, const std::string& name);
     
-    ~Animation() {
+    ~Animation();
 
-    }
+    void loadTracks(const std::vector<AnimationReader::AnimationTrack>& tracks);
+    std::vector<std::string> getRequiredResources() const;
 
 private:
     int _fps;
+    std::vector<AnimationReader::AnimationTrack> _tracks; // List of animation tracks
     int _frameCount; // Number of frames in the animation
     float _duration; // Duration of the animation in seconds
     std::string _name; // Name of the animation
