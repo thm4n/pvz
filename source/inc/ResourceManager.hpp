ac05e3e1c02aa8a78d2b7f94682e1440b5e09928
tamir hayne <tamir.hayne@cellebrite.com>
2025-06-29 09:38:51 +0300

    resource loading

--- source/inc/ResourceManager.hpp
+++ source/inc/ResourceManager.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_ttf.h"
+
 #include <iostream>
+#include <exception>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -10,8 +14,10 @@
 namespace fs = std::filesystem;
 
 #include "Texture.hpp"
+#include "Font.hpp"
 
 #include "Logger.hpp"
+#include "ResourceType.hpp"
 
 class ResourceManager {
 public:
@@ -20,13 +26,18 @@ public:
 	~ResourceManager();
 
 	// Load a resource from file
-	static void loadResource(const std::string& resourcePath);
+	static void loadResource(ResourceType& resourceType, const std::string& resourceName, const std::string& resourcePath);
+	static void loadImageResource(const std::string& resourceName, const std::string& resourcePath);
+	static void loadAudioResource(const std::string& resourceName, const std::string& resourcePath);
+	static void loadFontResource(const std::string& resourceName, const std::string& resourcePath, uint size);
+	static void loadTextTexture(const std::string& name, const std::string& text, const std::string& font, uint size, SDL_Color color);
 
 	static Texture* getResource(const std::string& resourcePath);
-
+	static Font* getFont(const std::string& resourceName, uint size);
 private:
 	static SDL_Renderer* _renderer; // Ref to the Pointer to the Graphics' SDL renderer
 	static std::map<const std::string, Texture*> _textures; // Map of loaded textures
+	static std::map<const std::string, Font*> _fonts; // Map of loaded fonts
 	
 	void freeAll();
 };
