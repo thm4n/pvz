bae82ea600cc6266d758e2eba5fbb76d349ab12a
tamir hayne <tamir.hayne@cellebrite.com>
2025-06-29 09:38:56 +0300

    resource type

---
+++ source/inc/ResourceType.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+enum class ResourceType {
+	Uninitialized = 0,  // Represents an uninitialized resource type
+	IMAGE,  // Represents an image resource
+	SOUND,  // Represents a sound resource
+	FONT    // Represents a font resource
+};
